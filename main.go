b33a782d23f594b28d3c4fd5341b73227f06c6ed
Maho Takara <takara9@gmail.com>
2018-01-16 05:14:18 +0000

    update

--- main.go
+++ main.go
@@ -7,10 +7,14 @@ import (
 )
 
 func handler(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "Hello World, %s!", request.URL.Path[1:])
+	fmt.Fprintf(writer, "Hello World, %s!\n", request.URL.Path[1:])
+	go_util.Logger.Printf("Hello World, %s!\n", request.URL.Path[1:])
 }
 
 func main() {
+	go_util.OpenLog("logfile.txt")
+	go_util.LoadConfig("config.json")
+
 	port := ":" + go_util.Config.TcpPort
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(port, nil)
